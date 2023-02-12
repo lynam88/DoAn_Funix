c9ca46260115d2cf24a1178527f94eaf2bced892
lynam88 <113892324+lynam88@users.noreply.github.com>
2023-02-13 06:07:45 +0700

    1

--- HV_SoftwarePackage/src/main/java/dao/UsersDAO.java
+++ HV_SoftwarePackage/src/main/java/dao/UsersDAO.java
@@ -21,13 +21,18 @@ public class UsersDAO {
 		Connection connection = new DBContext().getConnection();
 		List<Users> list = new ArrayList<>();
 		try {
-			String sql = "SELECT name, phone, email, address, registration_date, user_role "
-					+ "FROM Users "
-					+ "WHERE (name like N'%" + character + "%' OR phone = '" + character + "') "
-							+ "AND status = 1" + (searchStatus.equals("0") ? "" : " AND user_role = " + searchStatus);
+			String sql = "SELECT name, phone, email, address, registration_date, user_role " + "FROM Users "
+					+ "WHERE (name like N'%' + ? + '%' OR phone = ?) " + "AND status = 1"
+					+ (searchStatus.equals("0") ? "" : " AND user_role = ?");
 
-			Statement stmt = connection.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, character);
+			stmt.setString(2, character);
+			if (!searchStatus.equals("0")) {
+				stmt.setString(3, searchStatus);
+			}
+			ResultSet rs = stmt.executeQuery();
+	
 			this.noOfRecords = 0;
 			while (rs.next()) {
 				Users u = new Users();
@@ -53,25 +58,30 @@ public class UsersDAO {
 
 	}
 
-	public List<Users> getRecord(String character, String searchStatus, int pageNo, int recordPerPage) throws Exception {
+	public List<Users> getRecord(String character, String searchStatus, int pageNo, int recordPerPage)
+			throws Exception {
 		Connection connection = new DBContext().getConnection();
 		List<Users> list = new ArrayList<>();
-		try {			
+		try {
 			String sql = "SELECT name, phone, email, address, registration_date, user_role, COUNT(*) OVER() AS total"
-					+ " FROM Users"
-					+ " WHERE"
-					+ " status = 1"
-					+ " AND (name like N'%" + character + "%' OR phone = '" + character + "')";
+					+ " FROM Users" + " WHERE" + " status = 1" + " AND (name like N'%?%' OR phone = '?')";
 			if (!searchStatus.equals("0")) {
-				sql+= " AND user_role = " + searchStatus;
+				sql += " AND user_role = ?";
 			}
-			sql+= " ORDER BY registration_date DESC OFFSET ("
-					+ pageNo + "  - 1)*"
-					+ recordPerPage + " ROWS FETCH NEXT "
-					+ recordPerPage + " ROWS ONLY";		
+			sql += " ORDER BY registration_date DESC OFFSET (? - 1) * ? ROWS FETCH NEXT ? ROWS ONLY";
 
-			Statement stmt = connection.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, "%" + character + "%");
+			stmt.setString(2, character);
+			if (!searchStatus.equals("0")) {
+				stmt.setInt(3, Integer.parseInt(searchStatus));
+			}
+			stmt.setInt(4, pageNo);
+			stmt.setInt(5, recordPerPage);
+			stmt.setInt(6, recordPerPage);
+
+			ResultSet rs = stmt.executeQuery();
+			
 			while (rs.next()) {
 				Users u = new Users();
 				u.setName(rs.getString("name"));
@@ -83,6 +93,7 @@ public class UsersDAO {
 
 				list.add(u);
 			}
+
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
@@ -93,9 +104,13 @@ public class UsersDAO {
 		Connection connection = new DBContext().getConnection();
 		Users u = new Users();
 
-		Statement statement = connection.createStatement();
-		ResultSet rs = statement.executeQuery(
-				"SELECT * FROM Users WHERE (email = '" + id + "' OR phone = '" + id + "') AND password = '" + password + "' AND status = 1");
+		String sql = "SELECT * FROM Users WHERE (email = '?' OR phone = '?') AND password = '?' AND status = 1";
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setString(1, id);
+		stmt.setString(2, password);
+
+		ResultSet rs = stmt.executeQuery(sql);
+		stmt.close();
 
 		if (rs.next()) {
 			u.setName(rs.getString("name"));
@@ -104,29 +119,29 @@ public class UsersDAO {
 			u.setPassword(rs.getString("password"));
 			u.setAddress(rs.getString("address"));
 			u.setRegistration_date(rs.getDate("registration_date"));
-
 		}
 		return u;
+
 	}
 
 	public void deleteUser(List<Users> us) throws Exception {
 		Connection connection = new DBContext().getConnection();
-		
+
 		String sql = "BEGIN TRANSACTION\n";
-		sql += "UPDATE Users SET status = 0 WHERE user_role = 2 AND email in (?)\n";		
+		sql += "UPDATE Users SET status = 0 WHERE user_role = 2 AND email in (?)\n";
 		sql += "COMMIT TRANSACTION";
 		PreparedStatement stmt = connection.prepareStatement(sql);
 
 		for (Users u : us) {
 			if (u.getRole() == 1) {
-		        throw new Exception("Cannot delete ADMIN");
-		    }
+				throw new Exception("Cannot delete ADMIN");
+			}
 			stmt.setString(1, u.getEmail());
 		}
 
 		stmt.executeUpdate();
 		stmt.close();
-		
+
 	}
 
 }
